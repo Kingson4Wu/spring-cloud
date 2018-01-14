b3b06746a779a4f9c1cd89d098ac08c338c96aa2
kingson4wu <Kingson_Wu@163.com>
2018-01-14 16:58:56 +0800

    zuul filter

--- spring-cloud-zuul-api-gateway/src/main/java/com/kxw/microservice/zuul/apigateway/ZuulApiGatewayApplication.java
+++ spring-cloud-zuul-api-gateway/src/main/java/com/kxw/microservice/zuul/apigateway/ZuulApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.kxw.microservice.zuul.apigateway;
 
+import com.kxw.microservice.zuul.apigateway.filter.AccessFilter;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * http://www.spring4all.com/article/301
@@ -14,5 +16,10 @@ public class ZuulApiGatewayApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulApiGatewayApplication.class)
             .web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
